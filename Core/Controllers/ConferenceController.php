f563c6bbc507b5c70b67694091d65993eb81a5ba
XioRi007 <mariyaivanova@ukr.net>
2023-04-08 17:23:03 +0300

    fixing img loading

--- Core/Controllers/ConferenceController.php
+++ Core/Controllers/ConferenceController.php
@@ -90,10 +90,10 @@ class ConferenceController
     public function ImgAction()
     {
         $filename = substr($_SERVER['REQUEST_URI'], 5);
-        $file_path = 'uploads/' . $filename;
+        $file_path = '../uploads/' . $filename;
 
         if (!file_exists($file_path)) {
-            $file_path = 'uploads/default.png';
+            $file_path = '../uploads/default.png';
         }
         header('Content-Type: ' . mime_content_type($file_path));
         readfile($file_path);
