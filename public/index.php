7b6d9e01535520654be276be99bd64a1a744c879
XioRi007 <mariyaivanova@ukr.net>
2023-04-08 17:29:01 +0300

    mooving env to root folder

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
